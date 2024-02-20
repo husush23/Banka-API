77402d3ce2ec0810f7c7e88fbdb96ce3a812d4e1
husush23 <husseinkadare40@gmail.com>
2024-02-20 11:25:51 +0530

    cards services created

--- src/cards/cards.module.ts
+++ src/cards/cards.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CardsController } from './cards.controller';
 import { CardsService } from './cards.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Card } from './card.entity';
 
 @Module({
   controllers: [CardsController],
-  providers: [CardsService]
+  imports: [TypeOrmModule.forFeature([Card])],
+  providers: [CardsService],
 })
 export class CardsModule {}
--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,4 +1,36 @@
+/*eslint-disable*/
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Card } from './card.entity';
 
 @Injectable()
-export class CardsService {}
+export class CardsService {
+  constructor(
+    @InjectRepository(Card) private cardsRepository: Repository<Card>,
+  ) {}
+  findAllUsers(userId: string): Promise<Card[]> {
+    return this.cardsRepository.find({ where: { userId } });
+  }
+
+  findOneUser(userId: string): Promise<Card> {
+    return this.cardsRepository.findOneBy({ userId });
+  }
+
+  async createCard(card: Card): Promise<Card> {
+    return this.cardsRepository.save(card);
+  }
+
+  async updateCard(id: number, cardInfo: Partial<Card>): Promise<Card> {
+    const card = this.cardsRepository.findOneBy({ id });
+    if (card) {
+      await this.cardsRepository.update(id, cardInfo);
+      return this.cardsRepository.findOneBy({ id });
+    }
+    return null;
+  }
+
+  async deleteCard(id: number): Promise<void> {
+    await this.cardsRepository.delete(id);
+  }
+}
